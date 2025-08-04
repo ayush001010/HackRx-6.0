091e6888b07b5ec5cef9242ce2513298a461e31d
Abhirup-Choudhury <abhirup.choudhury2019@gmail.com>
2025-08-04 21:01:21 +0530

    modified:   retriever.py

--- retriever.py
+++ retriever.py
@@ -3,7 +3,7 @@ from langchain_chroma import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.vectorstores.base import VectorStoreRetriever
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
+from langchain_community.document_loaders import PyMuPDFLoader
 from document_manager import DocumentManager
 from pdf_loader import PDFLoader
 from docx_loader import DocxLoader
@@ -19,17 +19,22 @@ class VectorStoreProvider:
         file_path = self.manager.get_filepath()
         file_extension = os.path.splitext(file_path)[1].lower()
 
-        if file_extension == ".pdf":
-            loader = PDFLoader(file_path)
-        elif file_extension == ".docx":
-            loader = DocxLoader(file_path)
-        elif file_extension in [".eml", ".msg"]:
-            loader = EmailLoader(file_path)
-        else:
-            raise ValueError(f"Unsupported file type: {file_extension}")
-
+        loader = PyMuPDFLoader(
+            "./example_data/layout-parser-paper.pdf",
+            mode="single",
+        )
         print(f"Loading document with {type(loader).__name__}...")
         raw_documents = loader.load()
+        
+        # if file_extension == ".pdf":
+        #     loader = PDFLoader(file_path)
+        # elif file_extension == ".docx":
+        #     loader = DocxLoader(file_path)
+        # elif file_extension in [".eml", ".msg"]:
+        #     loader = EmailLoader(file_path)
+        # else:
+        #     raise ValueError(f"Unsupported file type: {file_extension}")
+
 
         if not raw_documents:
             raise ValueError(f"Could not load any content from {file_path}")
